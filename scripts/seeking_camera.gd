f5cd637ef84eacf80cd5eae8ab83824d9047f1c2
Andrej Lapin <lapin.andrej1@gmail.com>
2025-08-04 22:52:36 +0300

    fixed vibe code

--- scripts/seeking_camera.gd
+++ scripts/seeking_camera.gd
@@ -42,16 +42,7 @@ func _physics_process(delta: float) -> void:
 
 func get_mouse_world_position() -> Vector3:
 	var mouse_position := get_viewport().get_mouse_position()
-	var ray_origin := project_ray_origin(mouse_position)
-	var ray_direction := project_ray_normal(mouse_position)
-	var ray_length := 1000
-	var ray_end := ray_origin + ray_direction * ray_length
-	
-	var intersection := ground_plane.intersects_ray(ray_origin, ray_end) as Vector3
-	#print("Mouse world position: ", intersection)
-	if intersection == null:
-		return Vector3.ZERO
-	return intersection
+	return project_position(mouse_position, position.y)
 
 func seek_target(_delta: float) -> void:
 	global_position = Vector3(target.global_position.x, global_position.y, target.global_position.z)
