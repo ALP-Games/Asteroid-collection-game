a9c96094cb91a8678195afdc5ffdf3de00bfaeae
Andrej Lapin <lapin.andrej1@gmail.com>
2025-08-04 22:28:16 +0300

    finished poi icon

--- ui/gameplay_ui/pointer_ui.gd
+++ ui/gameplay_ui/pointer_ui.gd
@@ -12,6 +12,7 @@ var _collection_zone: CollectionZone
 func _ready() -> void:
 	pointer_pivot.visible = false
 	pointer_pivot.pivot_offset = Vector2(pointer_pivot.size.x / 2, pointer_pivot.size.y / 2)
+	po_i_icon.pivot_offset = Vector2(po_i_icon.size.x / 2, po_i_icon.size.y / 2)
 	_player_ship = get_tree().get_first_node_in_group("player")
 	_collection_zone = get_tree().get_first_node_in_group("collector")
 
@@ -26,5 +27,6 @@ func _physics_process(delta: float) -> void:
 		pointer_pivot.visible = true
 		var angle := Vector3.FORWARD.signed_angle_to(position_delta, Vector3.UP)
 		pointer_pivot.rotation = -angle
+		po_i_icon.rotation = angle
 	else:
 		pointer_pivot.visible = false
