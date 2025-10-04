0e622f8e1947d5446280cca49174dab2bc6d4908
Andrej Lapin <lapin.andrej1@gmail.com>
2025-10-04 13:19:35 +0300

    hook hit effect

--- actors/hook.gd
+++ actors/hook.gd
@@ -2,6 +2,8 @@ class_name Hook extends RopeSegment
 
 signal target_reached(target: RigidBody3D, attachment_joint: Node3D)
 
+const HOOK_HIT_ASTEROID = preload("uid://btw85yqdumem8")
+
 var hooked := false
 
 var hook_joint: PinJoint3D = null
@@ -21,6 +23,9 @@ func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 		var hookable := HookableComponent.core().get_from(colliding_object)
 		if not hookable:
 			continue
+		if colliding_object is Asteroid:
+			var velocity_direction := state.get_contact_local_velocity_at_position(index).normalized()
+			_instantiate_asteroid_hit(velocity_direction, contact_position)
 		hooked = true
 		attach_at_point(colliding_object, contact_position)
 		target_reached.emit(colliding_object, hook_joint)
@@ -29,6 +34,16 @@ func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 		return
 	#print("Contact count - ", contact_count)
 
+
+func _instantiate_asteroid_hit(velocity_direction: Vector3, contact_position: Vector3) -> void:
+	var asteroid_hit_effect := HOOK_HIT_ASTEROID.instantiate() as Node3D
+	get_tree().get_first_node_in_group("instantiated_root").add_child(asteroid_hit_effect)
+	asteroid_hit_effect.global_position = contact_position
+	asteroid_hit_effect.look_at(contact_position - velocity_direction)
+	var particles := asteroid_hit_effect.get_child(0) as GPUParticles3D
+	particles.emitting = true
+	particles.finished.connect(func():asteroid_hit_effect.queue_free())
+	#asteroid_hit_effect.
 
 func _on_child_entered_tree(node: Node) -> void:
 	if hooked and node is PinJoint3D:
