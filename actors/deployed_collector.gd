d006123ba38f08464f619a9e56e58076f0154e13
Andrej Lapin <lapin.andrej1@gmail.com>
2025-11-02 21:52:41 +0200

    animation updates

--- actors/deployed_collector.gd
+++ actors/deployed_collector.gd
@@ -1,17 +1,18 @@
 class_name DeployedCollector extends CollectionZone
 
 @onready var collection_area: Area3D = $CollectionArea
-@onready var animation_player: AnimationPlayer = $rocket_base_a2/AnimationPlayer
+@onready var collection_sound: AudioStreamPlayer3D = $CollectionSound
+@onready var animation_tree: AnimationTree = $AnimationTree
 
 
 func _ready() -> void:
 	collection_area.monitoring = false
-	animation_player.play(&"mobilecollector_active")
-	animation_player.animation_finished.connect(_on_collector_deployed, CONNECT_ONE_SHOT)
+	collection_sound.playing = false
+	animation_tree.animation_finished.connect(_on_collector_deployed, CONNECT_ONE_SHOT)
 
 
 func _on_collector_deployed(anim_name: StringName) -> void:
 	if anim_name != &"mobilecollector_active":
 		return
-	animation_player.play("Shreder")
+	collection_sound.playing = true
 	collection_area.monitoring = true
