64ffa6d34ec78b919b3137633a1aa9e4298a0215
Andrej Lapin <lapin.andrej1@gmail.com>
2025-11-07 18:57:15 +0200

    latch dependencies

--- actors/latch_mechanism.gd
+++ actors/latch_mechanism.gd
@@ -6,6 +6,9 @@ const MIN_DETATCH_DISTANCE: float = 0.9
 @export var retraction_acceleration: float = 50.0
 @export var strength_exponent: float = 2.0
 
+@export var dependant_latches: Array[LatchMechanism]
+@export var share_dependence: bool = true
+
 @onready var slider_joint_3d: SliderJoint3D = $SliderJoint3D
 @onready var latch: RigidBody3D = $Latch
 
@@ -19,6 +22,10 @@ var latched := true
 func _ready() -> void:
 	print("Slider length - ", _slider_length)
 	print("Latch position - ", latch.position.z)
+	if share_dependence:
+		for dependant_latch in dependant_latches:
+			if dependant_latch.dependant_latches.find(self) == -1:
+				dependant_latch.dependant_latches.append(self)
 
 func _physics_process(delta: float) -> void:
 	# we can't use global z, we need to normalize it or something
@@ -30,12 +37,23 @@ func _physics_process(delta: float) -> void:
 func _process_latch() -> void:
 	var protruded_amount := 0 - latch.position.z
 	#print("Latch position - ", latch.position.z)
-	if protruded_amount / _slider_length >= MIN_DETATCH_DISTANCE:
-		latched = false
-		slider_joint_3d.queue_free()
+	if read_to_unlatch():
+		var unlatch := true
+		for dependant_latch in dependant_latches:
+			if not dependant_latch.read_to_unlatch():
+				unlatch = false
+				break
+		if unlatch:
+			latched = false
+			slider_joint_3d.queue_free()
 		
 	#print("Protruded amount - ", protruded_amount)
 	if protruded_amount > 0:
 		var retraction_strength := pow(retraction_acceleration * protruded_amount / _slider_length, strength_exponent)
 		latch.apply_central_force(Vector3(0.0, 0.0, 1.0).rotated(Vector3.UP, latch.global_rotation.y) * latch.mass * retraction_strength)
 	#print("Latch velocity - ", latch.linear_velocity)
+
+
+func read_to_unlatch() -> bool:
+	var protruded_amount := 0 - latch.position.z
+	return protruded_amount / _slider_length >= MIN_DETATCH_DISTANCE
