5b5bc2c3815d76026e633339340ecbb61aad1d31
Andrej Lapin <lapin.andrej1@gmail.com>
2025-11-08 19:08:50 +0200

    latch visual effects

--- actors/latch_mechanism.gd
+++ actors/latch_mechanism.gd
@@ -5,12 +5,17 @@ const MIN_DETATCH_DISTANCE: float = 0.9
 
 @export var retraction_acceleration: float = 50.0
 @export var strength_exponent: float = 2.0
+@export var unlatch_impulse: float = 40.0
+@export var deflation_duration: float = 2.0
 
 @export var dependant_latches: Array[LatchMechanism]
 @export var share_dependence: bool = true
 
 @onready var slider_joint_3d: SliderJoint3D = $SliderJoint3D
 @onready var latch: RigidBody3D = $Latch
+@onready var gas_emission_effect: GPUParticles3D = $Housing/GasEmissionEffect
+@onready var gas_emission_effect_2: GPUParticles3D = $Housing/GasEmissionEffect2
+
 
 @onready var _slider_length := slider_joint_3d.get_param(SliderJoint3D.PARAM_LINEAR_LIMIT_UPPER) -\
 	slider_joint_3d.get_param(SliderJoint3D.PARAM_LINEAR_LIMIT_LOWER)
@@ -20,8 +25,11 @@ const MIN_DETATCH_DISTANCE: float = 0.9
 var latched := true
 
 func _ready() -> void:
-	print("Slider length - ", _slider_length)
-	print("Latch position - ", latch.position.z)
+	#print("Slider length - ", _slider_length)
+	#print("Latch position - ", latch.position.z)
+	gas_emission_effect.amount_ratio = 0.0
+	gas_emission_effect_2.amount_ratio = 0.0
+	#gas_emission_effect.amount = 0
 	if share_dependence:
 		for dependant_latch in dependant_latches:
 			if dependant_latch.dependant_latches.find(self) == -1:
@@ -46,7 +54,23 @@ func _process_latch() -> void:
 		if unlatch:
 			latched = false
 			slider_joint_3d.queue_free()
-		
+			latch.apply_central_impulse(Vector3(0.0, 0.0, -1.0).rotated(Vector3.UP, latch.global_rotation.y) * latch.mass * unlatch_impulse)
+			#gas_emission_effect.emitting = false
+			gas_emission_effect.amount_ratio = 1.0
+			gas_emission_effect_2.amount_ratio = 1.0
+			var deflation_tween := create_tween()
+			deflation_tween.tween_property(self, "gas_emission_effect:amount_ratio", 0.0, deflation_duration).\
+				set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+			deflation_tween.tween_callback(func():gas_emission_effect.emitting = false)
+			
+			var deflation_tween_2 := create_tween()
+			deflation_tween_2.tween_property(self, "gas_emission_effect_2:amount_ratio", 0.0, deflation_duration).\
+				set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+			deflation_tween_2.tween_callback(func():gas_emission_effect_2.emitting = false)
+	
+	gas_emission_effect.amount_ratio = protruded_amount / _slider_length
+	gas_emission_effect_2.amount_ratio = protruded_amount / _slider_length
+	
 	#print("Protruded amount - ", protruded_amount)
 	if protruded_amount > 0:
 		var retraction_strength := pow(retraction_acceleration * protruded_amount / _slider_length, strength_exponent)
