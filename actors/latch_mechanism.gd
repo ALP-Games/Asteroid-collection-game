14366a6d015ebfec3e3253d5f41d13f06d15e7cb
Andrej Lapin <lapin.andrej1@gmail.com>
2025-11-11 18:51:24 +0200

    latches sound effects

--- actors/latch_mechanism.gd
+++ actors/latch_mechanism.gd
@@ -8,14 +8,20 @@ const MIN_PROTRUSION_DISTANCE: float = 0.1
 @export var strength_exponent: float = 2.0
 @export var unlatch_impulse: float = 40.0
 @export var deflation_duration: float = 2.0
+@export_group("Sound Effects")
+@export var sound_max_volume_db: float = 0.0
+@export var sound_min_volume_db: float = -10.0
+@export var sound_max_pitch: float = 1.0
+@export var sound_min_pitch: float = 0.9
 
 @export var dependant_latches: Array[LatchMechanism]
 @export var share_dependence: bool = true
 
 @onready var slider_joint_3d: SliderJoint3D = $SliderJoint3D
 @onready var latch: RigidBody3D = $Latch
-@onready var gas_emission_effect: GPUParticles3D = $Housing/GasEmissionEffect
-@onready var gas_emission_effect_2: GPUParticles3D = $Housing/GasEmissionEffect2
+@onready var gas_emission_effects_side: Array[GPUParticles3D] = [$Housing/GasEmissionEffectLeft, $Housing/GasEmissionEffectRight]
+@onready var gas_emission_effect_front: GPUParticles3D = $Housing/GasEmissionEffectFront
+@onready var gas_sound: AudioStreamPlayer3D = $Housing/GasSound
 
 
 @onready var _slider_length := slider_joint_3d.get_param(SliderJoint3D.PARAM_LINEAR_LIMIT_UPPER) -\
@@ -28,15 +34,20 @@ var latched := true
 func _ready() -> void:
 	#print("Slider length - ", _slider_length)
 	#print("Latch position - ", latch.position.z)
-	gas_emission_effect.amount_ratio = 0.0
-	gas_emission_effect_2.amount_ratio = 0.0
-	#gas_emission_effect.amount = 0
+	for effect in gas_emission_effects_side:
+		effect.amount_ratio = 0.0
+	
+	gas_emission_effect_front.emitting = false
+	
+	gas_sound.playing = true
+	gas_sound.stream_paused = true
+	
 	if share_dependence:
 		for dependant_latch in dependant_latches:
 			if dependant_latch.dependant_latches.find(self) == -1:
 				dependant_latch.dependant_latches.append(self)
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	# we can't use global z, we need to normalize it or something
 	# or we can try relative position for now
 	# it could also be calculated once
@@ -57,28 +68,40 @@ func _process_latch() -> void:
 			slider_joint_3d.queue_free()
 			latch.apply_central_impulse(Vector3(0.0, 0.0, -1.0).rotated(Vector3.UP, latch.global_rotation.y) * latch.mass * unlatch_impulse)
 			#gas_emission_effect.emitting = false
-			gas_emission_effect.amount_ratio = 1.0
-			gas_emission_effect_2.amount_ratio = 1.0
-			var deflation_tween := create_tween()
-			deflation_tween.tween_property(self, "gas_emission_effect:amount_ratio", 0.0, deflation_duration).\
-				set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-			deflation_tween.tween_callback(func():gas_emission_effect.emitting = false)
 			
-			var deflation_tween_2 := create_tween()
-			deflation_tween_2.tween_property(self, "gas_emission_effect_2:amount_ratio", 0.0, deflation_duration).\
+			for effect in gas_emission_effects_side:
+				effect.amount_ratio = 0.0
+				effect.emitting = false
+			
+			gas_emission_effect_front.amount_ratio = 1.0
+			gas_emission_effect_front.emitting = true
+			
+			var deflation_tween := create_tween()
+			deflation_tween.tween_property(self, "gas_emission_effect_front:amount_ratio", 0.0, deflation_duration).\
 				set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-			deflation_tween_2.tween_callback(func():gas_emission_effect_2.emitting = false)
+			deflation_tween.tween_callback(func():gas_emission_effect_front.emitting = false)
+			
+			gas_sound.volume_db = sound_max_volume_db
+			var deflation_volume_tween := create_tween()
+			deflation_volume_tween.tween_property(self, "gas_sound:volume_db", sound_min_volume_db, deflation_duration).\
+				set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+			deflation_volume_tween.tween_callback(func():gas_sound.playing = false)
+			
+			var deflation_pitch_tween := create_tween()
+			deflation_pitch_tween.tween_property(self, "gas_sound:pitch_scale", sound_min_pitch, deflation_duration).\
+				set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 	
-	var emission_ratio: float = max(protruded_amount - MIN_PROTRUSION_DISTANCE, 0.0) / _slider_length - MIN_PROTRUSION_DISTANCE
-	gas_emission_effect.amount_ratio = emission_ratio
-	gas_emission_effect_2.amount_ratio = emission_ratio
+	var emission_ratio: float = clampf(protruded_amount - MIN_PROTRUSION_DISTANCE /  _slider_length - MIN_PROTRUSION_DISTANCE, 0.0, 1.0)
+	for effect in gas_emission_effects_side:
+		effect.amount_ratio = emission_ratio
 	
-	#print("Protruded amount - ", protruded_amount)
+	gas_sound.stream_paused = protruded_amount < MIN_PROTRUSION_DISTANCE
+	gas_sound.volume_db = lerp(sound_min_volume_db, sound_max_volume_db, emission_ratio)
+	gas_sound.pitch_scale = lerp(sound_min_pitch, sound_max_pitch, emission_ratio)
+	
 	if protruded_amount > 0:
 		var retraction_strength := pow(retraction_acceleration * protruded_amount / _slider_length, strength_exponent)
 		latch.apply_central_force(Vector3(0.0, 0.0, 1.0).rotated(Vector3.UP, latch.global_rotation.y) * latch.mass * retraction_strength)
-	#print("Latch velocity - ", latch.linear_velocity)
-
 
 func read_to_unlatch() -> bool:
 	var protruded_amount := 0 - latch.position.z
