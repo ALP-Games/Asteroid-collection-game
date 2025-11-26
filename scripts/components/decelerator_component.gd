b34a3fd0fe0fdcab7542172c3febec1bb072d079
Andrej Lapin <lapin.andrej1@gmail.com>
2025-11-26 15:43:16 +0200

    deceleration component angular decel

--- scripts/components/decelerator_component.gd
+++ scripts/components/decelerator_component.gd
@@ -5,6 +5,8 @@ extends Component
 @onready var parent_body: RigidBody3D = get_parent()
 
 @export var deceleration_amount: float = 10.0
+@export var stop_angular_amount: float = 1.0
+#@export var min_decel_for_effects: 
 @export var deceleration_sounds: Array[AudioStreamPlayer3D]
 @export var deceleration_effects: Array[GPUParticles3D]
 
@@ -24,6 +26,7 @@ func _physics_process(delta: float) -> void:
 	var negative_acceleration := -parent_body.linear_velocity.normalized() * deceleration_deficit
 	#print("Negative acceleration - ", negative_acceleration)
 	var enable_effects := decelerate and not is_zero_approx(parent_body.linear_velocity.length())
+	print("Velocity - ", parent_body.linear_velocity.length())
 	_enable_graphics(enable_effects)
 	_enable_sound(enable_effects)
 	if not decelerate:
@@ -34,6 +37,11 @@ func _physics_process(delta: float) -> void:
 	else:
 		mass = parent_body.mass
 	parent_body.apply_central_force(mass * negative_acceleration)
+	
+	var inverse_inertia = PhysicsServer3D.body_get_direct_state(parent_body.get_rid()).inverse_inertia
+	var reverse_angular_acceleration := -parent_body.angular_velocity.y / delta
+	reverse_angular_acceleration = clamp(reverse_angular_acceleration, -stop_angular_amount, stop_angular_amount)
+	parent_body.apply_torque(Vector3.UP * reverse_angular_acceleration / inverse_inertia)
 
 
 func _enable_graphics(enable: bool) -> void:
