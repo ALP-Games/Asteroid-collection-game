86c196d5337abff22eeded5adfda966f809823ce
Andrej Lapin <lapin.andrej1@gmail.com>
2025-12-03 23:56:01 +0200

    nicer pointer arrow, grinder script uid

--- ui/gameplay_ui/scripts/pointer_ui.gd
+++ ui/gameplay_ui/scripts/pointer_ui.gd
@@ -6,6 +6,7 @@ class_name PointerUI extends Control
 @export var display_distance: float = 30.0
 
 var _player_ship: PlayerShip
+var _camera: Camera3D
 var _collection_zone: CollectionZone
 
 
@@ -14,6 +15,7 @@ func _ready() -> void:
 	pointer_pivot.pivot_offset = Vector2(pointer_pivot.size.x / 2, pointer_pivot.size.y / 2)
 	po_i_icon.pivot_offset = Vector2(po_i_icon.size.x / 2, po_i_icon.size.y / 2)
 	_player_ship = get_tree().get_first_node_in_group("player")
+	_camera = (get_tree().get_first_node_in_group("camera") as FancyCameraArmature).camera_3d
 	# This should point to the closest?
 	# We should have a pointer to the shop
 	_collection_zone = get_tree().get_first_node_in_group("collector")
@@ -23,8 +25,8 @@ func _ready() -> void:
 	
 
 func _physics_process(_delta: float) -> void:
-	var position_delta := _collection_zone.global_position - _player_ship.global_position
-	#var distance_to_collection := _player_ship.global_position.distance_to(_collection_zone.global_position)
+	var position_delta := _collection_zone.global_position - _camera.global_position
+	position_delta.y = 0.0
 	if position_delta.length() >= display_distance:
 		pointer_pivot.visible = true
 		var angle := Vector3.FORWARD.signed_angle_to(position_delta, Vector3.UP)
